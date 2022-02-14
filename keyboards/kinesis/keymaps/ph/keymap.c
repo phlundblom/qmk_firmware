52329f5dcee38e9dbca9129ea31f1b213fa87c2a
PH Lundblom <ph.lundblom@sensenode.com>
2022-02-14 09:29:26 +0100

    Move F-keys from function row

--- keyboards/kinesis/keymaps/ph/keymap.c
+++ keyboards/kinesis/keymaps/ph/keymap.c
@@ -1,9 +1,11 @@
 #include QMK_KEYBOARD_H
 
-#define QWERTY 0 // Base qwerty
-#define EXTRA  1
+#define QWERTY   0 // Base qwerty
+#define FUNCTION 1
+#define EXTRA    2
 
 #define KC_EXEN LT(EXTRA, KC_END)
+#define KC_FNEN MO(FUNCTION)
 
 /****************************************************************************************************
 *
@@ -35,24 +37,44 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [QWERTY] = LAYOUT(
-           KC_ESC, KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8,
+           _______,_______,_______,_______,_______,_______,_______,_______,_______,
            KC_MINS,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,
            KC_TAB, KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,
            KC_LCTL,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,
            KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,
                    KC_NUBS,KC_GRV ,KC_LEFT,KC_RGHT,
-			   KC_LALT,KC_LCTL,
+			   KC_LALT,KC_ESC ,
                                     KC_HOME,
                            KC_BSPC,KC_DEL ,KC_EXEN,
-    KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_MPRV ,KC_MNXT  ,KC_MPLY, KC_FN0,_______,
+    _______,_______,_______,_______,_______ ,_______  ,_______,_______,_______,
 	KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_EQL ,
 	KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_LBRC,
 	KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT,
 	KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
 		KC_UP  ,KC_DOWN,KC_RBRC,KC_NUHS,
-           KC_RCTL,KC_ALGR,
+           KC_FNEN,KC_ALGR,
            KC_PGUP,
            KC_PGDN,KC_ENTER ,KC_SPC
+    ),
+[FUNCTION] = LAYOUT(
+           _______,_______,_______,_______,_______,_______,_______,_______,_______,
+           _______,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,
+           _______,_______,_______,_______,_______,_______,
+           _______,_______,KC_MPRV,KC_MNXT,KC_MPLY,_______,
+           _______,_______,_______,KC_F14 ,KC_F15 ,_______,
+                   _______,_______,_______,_______,
+			   _______,_______,
+                                    _______,
+                           _______,_______,_______,
+    _______,_______,_______,_______,_______ ,_______  ,_______,_______,_______,
+	KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,_______,
+	KC_F11 ,_______,_______,_______,_______,_______,
+	KC_F12 ,_______,_______,_______,_______,_______,
+	_______,_______,_______,_______,_______,_______,
+		_______,_______,_______,_______,
+           _______,_______,
+           _______,
+           _______,_______  ,_______
     ),
 [EXTRA] = LAYOUT(
            _______,_______,_______,_______,_______,_______,_______,_______,_______,
